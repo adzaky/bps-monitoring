52fec9beba20fe3dba2517eff0fd1596e24d5205
bpsaminah <bpsaminah@gmail.com>
2025-08-16 22:52:30 +0800

    feat: add ordering to all service data retrieval

--- src/services/api.js
+++ src/services/api.js
@@ -3,23 +3,31 @@ import supabase from "../lib/supabase";
 export const api = {
   libraryService: {
     getLibraryServiceData: async () => {
-      return await supabase.from("pst_customer").select();
+      return await supabase.from("pst_customer").select().order("created_at", { ascending: false });
     },
   },
   romantikService: {
     getRomantikStatisticalActivities: async () => {
-      return await supabase.from("romantik_statistical").select();
+      return await supabase.from("romantik_statistical").select().order("created_at", { ascending: false });
     },
   },
   silastikService: {
     getStatisticalTransactions: async () => {
-      return await supabase.from("silastik_transaction").select();
+      return await supabase.from("silastik_transaction").select().order("created_at", { ascending: false });
     },
     getConsultationStatistic: async () => {
-      return await supabase.from("silastik_transaction").select().ilike("need_type", "%Konsultasi%");
+      return await supabase
+        .from("silastik_transaction")
+        .select()
+        .ilike("need_type", "%Konsultasi%")
+        .order("created_at", { ascending: false });
     },
     getProductStatistic: async () => {
-      return await supabase.from("silastik_transaction").select().ilike("need_type", "%Permintaan Data%");
+      return await supabase
+        .from("silastik_transaction")
+        .select()
+        .ilike("need_type", "%Permintaan Data%")
+        .order("created_at", { ascending: false });
     },
   },
 };
