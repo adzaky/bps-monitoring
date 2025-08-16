bcecd7f3e04984a51ac8a52a5323de15bd72dbb3
bpsaminah <bpsaminah@gmail.com>
2025-08-16 23:15:55 +0800

    feat: enhance data retrieval by adding additional ordering to service queries

--- src/services/api.js
+++ src/services/api.js
@@ -3,31 +3,45 @@ import supabase from "../lib/supabase";
 export const api = {
   libraryService: {
     getLibraryServiceData: async () => {
-      return await supabase.from("pst_customer").select().order("created_at", { ascending: false });
+      return await supabase
+        .from("pst_customer")
+        .select()
+        .order("created_at", { ascending: false })
+        .order("visit_datetime", { ascending: false });
     },
   },
   romantikService: {
     getRomantikStatisticalActivities: async () => {
-      return await supabase.from("romantik_statistical").select().order("created_at", { ascending: false });
+      return await supabase
+        .from("romantik_statistical")
+        .select()
+        .order("created_at", { ascending: false })
+        .order("submission_date", { ascending: false });
     },
   },
   silastikService: {
     getStatisticalTransactions: async () => {
-      return await supabase.from("silastik_transaction").select().order("created_at", { ascending: false });
+      return await supabase
+        .from("silastik_transaction")
+        .select()
+        .order("created_at", { ascending: false })
+        .order("request_date", { ascending: false });
     },
     getConsultationStatistic: async () => {
       return await supabase
         .from("silastik_transaction")
         .select()
         .ilike("need_type", "%Konsultasi%")
-        .order("created_at", { ascending: false });
+        .order("created_at", { ascending: false })
+        .order("request_date", { ascending: false });
     },
     getProductStatistic: async () => {
       return await supabase
         .from("silastik_transaction")
         .select()
         .ilike("need_type", "%Permintaan Data%")
-        .order("created_at", { ascending: false });
+        .order("created_at", { ascending: false })
+        .order("request_date", { ascending: false });
     },
   },
 };
